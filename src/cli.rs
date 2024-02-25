e6ea722b0a5e8f496f5bcff1fc9858ba41ef9a97
sils <sils@sils.li>
2024-02-25 22:28:11 +0100

    refactor: use separate cli module

---
+++ src/cli.rs
@@ -0,0 +1,51 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "Yo'av Moshe",
+version = None,
+about = "A simple but detailed weather indicator for Waybar using wttr.in",
+long_about = None)
+]
+pub struct Args {
+    #[arg(
+        long,
+        default_value = "temp_C",
+        help = "decide which current_conditions key will be shown on waybar"
+    )]
+    pub main_indicator: String,
+
+    #[arg(
+        long,
+        help = "optional expression that will be shown instead of main indicator. current_conditions keys surrounded by {} can be used. example:\n\
+        \"{ICON}{temp_C}({FeelsLikeC})\" will be transformed to \"text\":\"🌧️0(-4)\" in output"
+    )]
+    pub custom_indicator: Option<String>,
+
+    #[arg(
+        long,
+        default_value = "%Y-%m-%d",
+        help = "formats the date next to the days. see https://docs.rs/chrono/latest/chrono/format/strftime/index.html"
+    )]
+    pub date_format: String,
+
+    #[arg(long, help = "pass a specific location to wttr.in")]
+    pub location: Option<String>,
+
+    #[arg(
+        long,
+        help = "shows the icon on the first line and temperature in a new line"
+    )]
+    pub vertical_view: bool,
+
+    #[arg(
+        long,
+        help = " show a shorter description next to each hour, like 7° Mist instead of 7° Mist, Overcast 81%, Sunshine 17%, Frost 15%"
+    )]
+    pub hide_conditions: bool,
+
+    #[arg(long, help = "display time in AM/PM format")]
+    pub ampm: bool,
+
+    #[arg(long, help = "use fahrenheit instead of celsius")]
+    pub fahrenheit: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use clap::Parser;
 use reqwest::blocking::Client;
 use serde_json::{json, Map, Value};
 
+use crate::cli::Args;
+
+mod cli;
+
 const WEATHER_CODES: &[(i32, &str)] = &[
     (113, "☀️"),
     (116, "🌤️"),
@@ -75,56 +79,6 @@ const WEATHER_CODES: &[(i32, &str)] = &[
 ];
 
 const ICON_PLACEHOLDER: &str = "{ICON}";
-
-#[derive(Parser, Debug)]
-#[command(author = "Yo'av Moshe",
-version = None,
-about = "A simple but detailed weather indicator for Waybar using wttr.in",
-long_about = None)
-]
-struct Args {
-    #[arg(
-        long,
-        default_value = "temp_C",
-        help = "decide which current_conditions key will be shown on waybar"
-    )]
-    main_indicator: String,
-
-    #[arg(
-        long,
-        help = "optional expression that will be shown instead of main indicator. current_conditions keys surrounded by {} can be used. example:\n\
-        \"{ICON}{temp_C}({FeelsLikeC})\" will be transformed to \"text\":\"🌧️0(-4)\" in output"
-    )]
-    custom_indicator: Option<String>,
-
-    #[arg(
-        long,
-        default_value = "%Y-%m-%d",
-        help = "formats the date next to the days. see https://docs.rs/chrono/latest/chrono/format/strftime/index.html"
-    )]
-    date_format: String,
-
-    #[arg(long, help = "pass a specific location to wttr.in")]
-    location: Option<String>,
-
-    #[arg(
-        long,
-        help = "shows the icon on the first line and temperature in a new line"
-    )]
-    vertical_view: bool,
-
-    #[arg(
-        long,
-        help = " show a shorter description next to each hour, like 7° Mist instead of 7° Mist, Overcast 81%, Sunshine 17%, Frost 15%"
-    )]
-    hide_conditions: bool,
-
-    #[arg(long, help = "display time in AM/PM format")]
-    ampm: bool,
-
-    #[arg(long, help = "use fahrenheit instead of celsius")]
-    fahrenheit: bool,
-}
 
 fn main() {
     let args = Args::parse();
